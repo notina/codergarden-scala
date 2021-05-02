84c3687127aab478a50e612c8e5062df3d54bcb9
notina <valentina.catte@gmail.com>
2021-05-02 19:27:26 +0200

    Update ApplicationSpec.scala

--- src/test/scala/com/codergaden/ex2/ApplicationSpec.scala
+++ src/test/scala/com/codergaden/ex2/ApplicationSpec.scala
@@ -7,23 +7,23 @@ import org.scalatest.matchers.should.Matchers._
 class ApplicationSpec extends AnyFlatSpec with TypeCheckedTripleEquals {
 
   "The wordCounter with an empty string" should "get an empty Map" in {
-    Application.wordCounter("") shouldEqual Map()
+    WordsCount.wordCounter("") shouldEqual Map()
   }
 
   "The wordCounter with a string containing more occurrence of each letter" should "get a Map containing word's unique letter and its occurrence " in {
-    Application.wordCounter("Giorgio") shouldEqual Map('g' -> 2, 'i' -> 2, 'o' -> 2, 'r' -> 1)
+    WordsCount.wordCounter("Giorgio") shouldEqual Map('g' -> 2, 'i' -> 2, 'o' -> 2, 'r' -> 1)
   }
 
   "The wordWithCount with an empty string"  should "get an empty String " in {
-    Application.wordWithCount(("")) shouldEqual ""
+    WordsCount.wordWithCount(("")) shouldEqual ""
   }
 
   "The wordWithCount with a string containing more occurrence of each letter" should "get a String containing the word and all word's unique letters and their occurrences " in {
-    Application.wordWithCount(("Giorgio")) shouldEqual "Giorgio -> (g, 2) (i, 2) (o, 2) (r, 1)"
+    WordsCount.wordWithCount(("Giorgio")) shouldEqual "Giorgio -> (g, 2) (i, 2) (o, 2) (r, 1)"
   }
 
   "Given a string to the wordCounter, the length of the string" should " equal to the sum of all the occurrences in the Map " in {
-    "Giorgio".length shouldEqual Application.wordCounter("Giorgio").foldLeft(0.0)(_+_._2)
+    "Giorgio".length shouldEqual WordsCount.wordCounter("Giorgio").foldLeft(0.0)(_+_._2)
   }
 
 }
