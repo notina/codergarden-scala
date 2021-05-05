62bcc54972ea4d41e3e21e18b50358580178dec0
notina <valentina.catte@gmail.com>
2021-05-05 10:48:12 +0200

    Update ApplicationSpec.scala

--- src/test/scala/com/codergaden/ex2/ApplicationSpec.scala
+++ src/test/scala/com/codergaden/ex2/ApplicationSpec.scala
@@ -3,8 +3,9 @@ package com.codergaden.ex2
 import org.scalactic.TypeCheckedTripleEquals
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class ApplicationSpec extends AnyFlatSpec with TypeCheckedTripleEquals {
+class ApplicationSpec extends AnyFlatSpec  with ScalaCheckPropertyChecks {
 
   "The wordCounter with an empty string" should "get an empty Map" in {
     WordsCount.wordCounter("") shouldEqual Map()
@@ -26,4 +27,9 @@ class ApplicationSpec extends AnyFlatSpec with TypeCheckedTripleEquals {
     "Giorgio".length shouldEqual WordsCount.wordCounter("Giorgio").foldLeft(0.0)(_+_._2)
   }
 
+  "given any String, the sum of the result values" should "be the sum of the word length" in {
+    forAll { (word: String) =>
+      word.length shouldEqual WordsCount.wordCounter(word).foldLeft(0)(_ + _._2)
+    }
+  }
 }
