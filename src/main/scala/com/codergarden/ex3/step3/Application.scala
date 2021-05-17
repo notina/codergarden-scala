ab54b6ff620261cb85631fc6651cf604183032bc
notina <valentina.catte@gmail.com>
2021-05-17 19:15:07 +0200

    Update Application.scala

--- src/main/scala/com/codergarden/ex3/step3/Application.scala
+++ src/main/scala/com/codergarden/ex3/step3/Application.scala
@@ -10,13 +10,10 @@ object Application {
     val apple2x1 = Shop.OneFruitFree(Shop.Apple, 2)
     val orange3x1 = Shop.OneFruitFree(Shop.Orange, 3)
     val banana2x1 = Shop.OneFruitFree(Shop.Banana, 2)
-    val bananaAppleFreeBundle = Shop.cheapestBundleFree(List(Shop.Apple,Shop.Banana))
+    val bananaAppleFreeBundle = Shop.CheapestBundleFree(List(Shop.Apple,Shop.Banana))
 
     val offers = List(apple2x1, orange3x1, banana2x1, bananaAppleFreeBundle)
 
-    println(s"SC Total cost: £${Shop.Checkout(shoppingCart, offers).cost} " )
+    println(s"SC Total cost: £${Shop.checkout(shoppingCart, offers)} " )
   }
 }
-
-
-
